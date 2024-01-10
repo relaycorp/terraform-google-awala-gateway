344ec3715feb22021bde9a081537c2e64635351a
Gus Narea <gnarea@users.noreply.github.com>
2024-01-10 17:24:11 +0000

    feat: Configure load balancer

--- lb.tf
+++ lb.tf
@@ -11,6 +11,9 @@ module "load_balancer" {
   random_certificate_suffix       = true # In case the domain changes
   managed_ssl_certificate_domains = [var.pohttp_server_domain, var.poweb_server_domain]
 
+  create_url_map = false
+  url_map        = google_compute_url_map.main.self_link
+
   backends = {
     pohttp = {
       description = "PoHTTP"
@@ -48,6 +51,29 @@ module "load_balancer" {
   }
 
   http_forward = false
+}
+
+resource "google_compute_url_map" "main" {
+  name            = "gateway-${var.instance_name}"
+  default_service = module.load_balancer.backend_services["pohttp"].self_link
+
+  host_rule {
+    hosts        = [var.pohttp_server_domain]
+    path_matcher = "pohttp"
+  }
+  path_matcher {
+    name            = "pohttp"
+    default_service = module.load_balancer.backend_services["pohttp"].self_link
+  }
+
+  host_rule {
+    hosts        = [var.poweb_server_domain]
+    path_matcher = "poweb"
+  }
+  path_matcher {
+    name            = "poweb"
+    default_service = module.load_balancer.backend_services["poweb"].self_link
+  }
 }
 
 resource "google_compute_ssl_policy" "main" {
