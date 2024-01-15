416858734ec70b2b0de85b264f6df897f10924b6
Gus Narea <gnarea@users.noreply.github.com>
2024-01-15 13:32:17 +0000

    chore(example): Remove reference to VeraId

--- examples/basic/mongodb.tf
+++ examples/basic/mongodb.tf
@@ -4,7 +4,7 @@ locals {
 
 resource "mongodbatlas_serverless_instance" "main" {
   project_id = var.mongodbatlas_project_id
-  name       = "veraid-authority"
+  name       = "gateway"
 
   provider_settings_backing_provider_name = "GCP"
   provider_settings_provider_name         = "SERVERLESS"
@@ -13,6 +13,6 @@ resource "mongodbatlas_serverless_instance" "main" {
 
 resource "mongodbatlas_project_ip_access_list" "main" {
   project_id = var.mongodbatlas_project_id
-  comment    = "See https://github.com/relaycorp/terraform-google-veraid-authority/issues/3"
+  comment    = "See https://github.com/relaycorp/cloud-gateway/issues/95"
   cidr_block = "0.0.0.0/0"
 }
